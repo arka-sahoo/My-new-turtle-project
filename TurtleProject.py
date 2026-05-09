50cc21fe4588b546f7d6fcb5353ce5cd84897518
Arka Sahoo <arkasahoo2005@gmail.com>
2026-05-09 11:22:51 -0500

    Added code for the turtle program

--- TurtleProject.py
+++ TurtleProject.py
@@ -1,1 +1,16 @@
-print("My Neew Turtle Project")
+import turtle
+
+screen = turtle.screen
+screen.bgcolor("black")
+
+pen = turtle.Turtle()
+pen.speed(0)
+
+colors = ["red", "yellow", "blue", "green", "orange", "purple"]
+
+for i in range(100):
+    pen.color(colors[i % len(colors)])
+    pen.forward(i * 2)
+    pen.right(50)
+
+turtle.done()
